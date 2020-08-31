7fa0ec07107676cd73d85292ce749a8a150e437c
lorenzovon <lazyprogrammerx@gmail.com>
2020-09-01 05:19:25 +0530

    Navbar Header Title

---
+++ src/PlayGround/Playground.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../Static/NavBar' ;
+import ManageCampaigns from '../Static/ManageCampaigns' ;
+
+function Playground() {
+  return (
+    <div className="Playground">
+      <NavBar />     
+      <ManageCampaigns /> 
+    </div>
+  );
+}
+
+export default Playground;
---
+++ src/Static/ManageCampaigns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ManageCampaigns.css';
+
+function ManageCampaigns() {
+  return (
+    <h1 className="ManageCampaigns" >
+        Manage Campaigns
+    </h1>
+  );
+}
+
+export default ManageCampaigns;
